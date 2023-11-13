f0c1fa8443bc8b0ff70a517ca9c4d62a66f6e762
Jakeer040694 <101064440+Jakeer040694@users.noreply.github.com>
2023-11-13 15:50:31 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -8,7 +8,7 @@ sonarqubeScanner({
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
 	    'sonar.login': 'squ_dd9c695804fe2d2f68846dc0fe99b20f7d906d88',
 	    //'sonar.login': 'admin',
-	    //'sonar.password': 'admin',
+	    //'sonar.password': 'Sonar',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
